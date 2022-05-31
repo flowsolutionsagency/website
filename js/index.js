c89120af30dff8789bda93ae8c6f5c86e20c1995
flowsolutionsagency <106607768+flowsolutionsagency@users.noreply.github.com>
2022-05-31 11:58:06 -0400

    add files via upload -netlify

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+
+
+var animation = bodymovin.loadAnimation({
+
+	container: document.getElementById('strat-animation'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'json/Branding_Animation.json'
+
+	
+});
+
+var animation = bodymovin.loadAnimation({
+
+	container: document.getElementById('ux-animation'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'json/Web_Animation.json'
+
+	
+});
+
+var animation = bodymovin.loadAnimation({
+
+	container: document.getElementById('seo-animation'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'json/SEO_Animation.json'
+
+	
+});
+
+
+
+var animation = bodymovin.loadAnimation({
+
+	container: document.getElementById('mobile-animation'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'json/Mobile_Animation.json'
+
+	
+})
